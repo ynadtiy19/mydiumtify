399acd49813caab9b9075ff5cdbdb1f1d00308ed
maludeuu <limoree28@gmail.com>
2024-08-24 12:18:54 +0800

    uuu

---
+++ routes/medium/search.dart
@@ -0,0 +1,139 @@
+import 'dart:convert';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:http/http.dart' as http;
+import 'package:json_path/json_path.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  //medium/search?q=apple%20iphone%2013&pageindex=3
+  // Access the incoming request.
+  final request = context.request;
+
+  // Access the query parameters as a `Map<String, String>`.
+  final params = request.uri.queryParameters;
+
+  // Get the value for the key `name`.
+  // Default to `there` if there is no query parameter.
+  var query = params['q'] ?? 'apple iphone 13';
+  var pageindex = params['pageindex'] ?? '3';
+  Future<String?> fetchData(String url) async {
+    try {
+      // 发送 HTTP GET 请求
+      var response = await http.get(Uri.parse(url));
+
+      // 检查响应状态码是否为成功(200)
+      if (response.statusCode == 200) {
+        // 返回 HTML 内容
+        return response.body;
+      } else {
+        // 如果响应状态码不是 200，返回 null
+        return null;
+      }
+    } catch (e) {
+      // 如果发生任何错误，返回 null
+      return null;
+    }
+  }
+
+  var jsonresult = await fetchData(
+      "https://readmedium.com/api/search-posts?query=$query&pageIndex=$pageindex");
+  //响应纯json
+
+  // 假设 jsonresult 是通过 fetchData 获取的 JSON 响应并已被解析为一个 Map
+  var json = jsonDecode(jsonresult!);
+
+  List<MediaInfo> mediaInfoList = [];
+
+  // 将 previewInfos 转换为 List
+  List<dynamic> previewInfos = json['previewInfos'] as List<dynamic>;
+
+// 遍历 JSON 数据并创建 MediaInfo 实例
+  for (var data in previewInfos) {
+    final uniqueSlug =
+        JsonPath(r'$.uniqueSlug').read(data).first.value.toString();
+    final title = JsonPath(r'$.title').read(data).first.value.toString();
+    final subtitle = JsonPath(r'$.subtitle').read(data).first.value.toString();
+    final name =
+        JsonPath(r'$.authorInfo.name').read(data).first.value.toString();
+
+    // 获取并修改 avatarUrl
+    String avatarUrl =
+        JsonPath(r'$.authorInfo.avatarUrl').read(data).first.value.toString();
+    avatarUrl = avatarUrl.replaceFirst(
+        'miro.medium.com', 'cdn-images-1.readmedium.com');
+
+    // 获取并修改 postImg
+    String postImg = JsonPath(r'$.postImg').read(data).first.value.toString();
+    postImg =
+        postImg.replaceFirst('miro.medium.com', 'cdn-images-1.readmedium.com');
+
+    final readingTime =
+        JsonPath(r'$.readingTime').read(data).first.value.toString();
+    final createdAt =
+        JsonPath(r'$.createdAt').read(data).first.value.toString();
+    final isEligibleForRevenue =
+        JsonPath(r'$.isEligibleForRevenue').read(data).first.value.toString();
+
+    // 创建 MediaInfo 实例并添加到列表中
+    MediaInfo mediaInfo = MediaInfo(
+      uniqueSlug: uniqueSlug,
+      title: title,
+      subtitle: subtitle,
+      name: name,
+      avatarUrl: avatarUrl, // 使用修改后的 avatarUrl
+      postImg: postImg, // 使用修改后的 postImg
+      readingTime: readingTime,
+      createdAt: createdAt,
+      isEligibleForRevenue: isEligibleForRevenue,
+    );
+
+    mediaInfoList.add(mediaInfo);
+  }
+
+// 使用 toJson 方法将 MediaInfo 列表转换为 Map 列表
+  List<Map<String, dynamic>> jsonResponse =
+      mediaInfoList.map((media) => media.toJson()).toList();
+
+// 返回 JSON 响应
+  return Response.json(body: jsonResponse);
+}
+
+class MediaInfo {
+  final String uniqueSlug;
+  final String title;
+  final String subtitle;
+  final String name;
+  final String avatarUrl;
+  final String postImg;
+  final String readingTime;
+  final String createdAt;
+  final String isEligibleForRevenue;
+
+  MediaInfo({
+    required this.uniqueSlug,
+    required this.title,
+    required this.subtitle,
+    required this.name,
+    required this.avatarUrl,
+    required this.postImg,
+    required this.readingTime,
+    required this.createdAt,
+    required this.isEligibleForRevenue,
+  });
+
+  @override
+  // 将 MediaInfo 转换为 Map
+  Map<String, dynamic> toJson() {
+    return {
+      'uniqueSlug': uniqueSlug,
+      'title': title,
+      'subtitle': subtitle,
+      'name': name,
+      'avatarUrl': avatarUrl,
+      'postImg': postImg,
+      'readingTime': readingTime,
+      'createdAt': createdAt,
+      'isEligibleForRevenue': isEligibleForRevenue,
+    };
+  }
+}
