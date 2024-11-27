f6d9f5107fd63a6efc6ab02ffefe95eb2e510379
49241036 <162901184+ynadtiy19@users.noreply.github.com>
2024-11-27 21:03:53 +0800

    Update pinterestKeywords.dart

--- routes/pinterestKeywords.dart
+++ routes/pinterestKeywords.dart
@@ -13,7 +13,7 @@ Future<Response> onRequest(RequestContext context) async {
       'https://europe-west3-pingenerator-43a15.cloudfunctions.net/api/pinterestAPI/findSuggestedKeywordsAndVolumes');
   final body = jsonEncode({
     'access_token':
-        'pina_AEAZ25IWAA5LMAQAGAAGWDLGJ2K33EQBACGSPQ3ZCNFPLWXB7HEMVWMBAMQL6252W6K5KBREKXKF6SPXPTMPWSFF3XZUE6AA',
+        'pina_AEAZ25IWAA5LMAQAGAAGWDIYQS2G3EYBACGSPNSSNHKY7X6HXMRO7TV74ZQW2V3B4Q6ZLWPKD5WOCY2RMHGSNYGXUPLKRCIA',
     'searchTerm': searchTerm,
     'sortBy': sortBy
   });
