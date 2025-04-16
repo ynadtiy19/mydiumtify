443fd43155f432d958743560ee6b9049337b934e
ynadtiy <fluteoceanuuy@gmail.com>
2025-04-16 17:30:15 +0800

    ok

--- routes/imagebytes.dart
+++ routes/imagebytes.dart
@@ -4,17 +4,36 @@ import 'package:dart_frog/dart_frog.dart';
 import 'package:http/http.dart' as http;
 
 Future<Response> onRequest(RequestContext context) async {
-  //https://mydiumtify.globeapp.dev/imagebytes?q=一条小溪&rebackimg=true
+  //https://mydiumtify.globeapp.dev/imagebytes?q=%E4%B8%80%E6%9D%A1%E5%B0%8F%E6%BA%AA&rebackimg=true
 
   final params = context.request.uri.queryParameters;
   final query = params['q'] ?? '一条小河';
   final rebackimg = params['rebackimg']?.toLowerCase() == 'true';
 
   final headers = {
+    'accept': '*/*',
+    'accept-encoding': 'gzip, deflate, br, zstd',
+    'accept-language': 'zh-CN,zh;q=0.9',
     'authorization':
         'Bearer ya29.a0AZYkNZjIKZYDBE7P4V42X16j9ab9zJ5AD7dpTVpB6CKoV0-HiDfa2jE6hSv0j7LnmUDAIznSNdMF4hL9y3G7yfu_q5inEPVMTHOHtV8292ymF6Vqq-bZRWdZcZA8pw55kKkpfLnOa8u2Ur_cRZ5n5qW5fwj51U_sTNwVL4voTLNJ_4863SSXese_LyZNICko_Qn3W0EQ6pdbkVDQ1aUwdXnkKsLQ4ASByzlMTtdlMq9-Oy3x9wtdRRx-LIRNf8nMlFiWwsQn3g3JV3vbyYamdix-NZDphTNDHjUkkNk1-PFNADk06SOlGgJFiV2R-aj8nwvCmDkdKgg1QFYlygr-mpn-wxtf9K-Z_gKgQNpZnFaliu3xSLwSwS8R_2ulT013j6AyTTQDznvhPpn3zpvwMpjhx74RO_UYUX8LOFRoaCgYKAQQSARISFQHGX2Mi33Vg6egBm6iXOLaoeRus9Q0431',
-    'Content-Type': 'application/json'
+    'cache-control': 'no-cache',
+    'content-length': '729',
+    'content-type': 'text/plain;charset=UTF-8',
+    'origin': 'https://labs.google',
+    'pragma': 'no-cache',
+    'priority': 'u=1, i',
+    'referer': 'https://labs.google/',
+    'sec-ch-ua':
+        '"Chromium";v="136", "Microsoft Edge";v="136", "Not.A/Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
   };
+
   final imagebody = jsonEncode({
     'userInput': {
       'candidatesCount': 1,
