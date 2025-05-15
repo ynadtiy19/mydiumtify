aaedabe9bbdfe186a4290b4ba2e749aa6a7d4ebb
ynadtiy <162901184+ynadtiy19@users.noreply.github.com>
2025-05-15 20:17:07 +0800

    ok

--- routes/globechat.dart
+++ routes/globechat.dart
@@ -1,22 +1,35 @@
+import 'package:ai_chat_plus/ai_chat_plus.dart';
 import 'package:dart_frog/dart_frog.dart';
-import 'package:globe_ai/globe_ai.dart';
 
 Future<Response> onRequest(RequestContext context) async {
+  //http://localhost:8080/globechat?q=你是chatgpt吗？
   //https://mydiumtify.globeapp.dev/globechat?q=你是chatgpt吗？
   // 获取查询参数
   final params = context.request.uri.queryParameters;
   final query = params['q'] ?? 'hello how are you doing?🥰🥰';
 
   try {
-    final result = await generateText(
-      model: openai.chat('gpt-4o'),
-      prompt: query,
+    final config = AIModelConfig(
+      provider: AIProvider.openAI,
+      apiKey:
+          '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',
+      modelId: OpenAIModel.gpt4Turbo.modelId,
     );
+
+    final aiService = AIServiceFactory.createService(AIProvider.openAI);
+    await aiService.initialize(config);
+
+    // Generate response
+    final response = await aiService.generateResponse(query);
+    // final result = await generateText(
+    //   model: openai.chat('gpt-4o'),
+    //   prompt: query,
+    // );
 
     return Response.json(
       body: {
         'query': query,
-        'response': result,
+        'response': response,
       },
     );
   } catch (e, stackTrace) {
